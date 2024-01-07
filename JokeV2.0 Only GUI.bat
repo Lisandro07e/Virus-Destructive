9dcff25815522647067ecea6e86e8640d89a8be2
Lisandro 07 Files And Windows <97541842+Lisandro07e@users.noreply.github.com>
2024-01-07 20:02:20 -0300

    New Update 2.0 For Old Versions and A New FIle
    ¡Warning Only in a Virtual Machine No In a Real Machine Thanks!

---
+++ JokeV2.0 Only GUI.bat
@@ -0,0 +1,14 @@
+msg * This is only The GUI Your PC is Good not is for Norning
+msg * Bye
+start cmd
+start notepad.exe
+start winver.exe
+start conhost.exe
+start iexplore.exe
+start wmplayer.exe
+start mmc.exe
+start services.msc
+start taskmgr.exe
+start regedit.exe
+start control
+start gpedit.msc 

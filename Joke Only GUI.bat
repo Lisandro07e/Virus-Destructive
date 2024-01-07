8ca2604d99c9365df1f3fbdbcf3760224ca31fac
Lisandro 07 Files And Windows <97541842+Lisandro07e@users.noreply.github.com>
2024-01-06 23:46:57 -0300

    Joke Only Gui.bat
    Your system is fine. The system was not deleted The System32 It's not very dangerous

---
+++ Joke Only GUI.bat
@@ -0,0 +1,22 @@
+msg * This is only The GUI Your PC is Good not is for Norning
+msg * Bye
+start cmd
+start wusa.exe
+start imageres.dll
+start notepad.exe
+start winver.exe
+start conhost.exe
+start iexplore.exe
+start wmplayer.exe
+start mmc.exe
+start services.msc
+start shell32.dll
+start taskmgr.exe
+start regedit.exe
+start control
+start gpedit.exe
+start aadjcsp.dll
+start acmigration.dll
+start taskschd.msc
+start taskschd.dll
+start slui.exe 

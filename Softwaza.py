ad73e3340b3c2f45b94dc977d71cfc9399ff0c96
Lisandro 07 Files And Windows <97541842+Lisandro07e@users.noreply.github.com>
2024-03-30 18:23:34 -0300

    Add files via upload
    For Linux

---
+++ Softwaza.py
@@ -0,0 +1,14 @@
+import string
+import time
+text = 'Hello World'
+temp =
+for ch in text:
+for i in string.printable
+if ich or ch ==
+    time.sleep(0.01)
+        print(temp+i)
+temp +=ch
+    break
+else:
+    time.sleep(0.01)
+    print(temp+i)

